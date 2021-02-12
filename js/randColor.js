aa4979df244a7a416cdcabc2be3e5709d540a171
Justin <justin@Alma.attlocal.net>
2021-02-12 13:47:12 -0800

    Added third color scheme.

--- js/randColor.js
+++ js/randColor.js
@@ -1,7 +1,7 @@
 // Get the root element
 var r = document.querySelector(':root');
 
-randomNumber = Math.round(Math.random());
+randomNumber = Math.round(Math.random()*2);
 
 window.onload = function() {
 	if (randomNumber == 0) {
@@ -10,8 +10,13 @@ window.onload = function() {
 		r.style.setProperty('--style-text-color', '#FF3300');
 	}
 	if (randomNumber == 1) {
-		r.style.setProperty('--main-bg-color', '#CCFFDD');
+		r.style.setProperty('--main-bg-color', '#EAFFFD');
 		r.style.setProperty('--main-text-color', '#333');
 		r.style.setProperty('--style-text-color', '#EE0066');
+	}
+	if (randomNumber == 2) {
+		r.style.setProperty('--main-bg-color', '#CCFFCC');
+		r.style.setProperty('--main-text-color', '#333');
+		r.style.setProperty('--style-text-color', '#990099');
 	}
 }
