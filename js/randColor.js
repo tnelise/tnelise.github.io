9b70c32eb0d075273d22928ceb7ce67fcef5a32b
Justin <justin@Alma.attlocal.net>
2021-02-16 20:52:47 -0800

    Changed random color probabilities.

--- js/randColor.js
+++ js/randColor.js
@@ -1,22 +1,22 @@
 // Get the root element
 var r = document.querySelector(':root');
 
-randomNumber = Math.round(Math.random()*2);
+randomNumber = Math.round(Math.random()*3);
 
 window.onload = function() {
-	if (randomNumber == 0) {
+	if (randomNumber == 0 || randomNumber == 1) {
 		r.style.setProperty('--main-bg-color', '#FFFFCC');
 		r.style.setProperty('--main-text-color', '#333');
 		r.style.setProperty('--style-text-color', '#FF3300');
 	}
-	if (randomNumber == 1) {
+	if (randomNumber == 2) {
 		r.style.setProperty('--main-bg-color', '#EAFFFD');
 		r.style.setProperty('--main-text-color', '#333');
 		r.style.setProperty('--style-text-color', '#EE0066');
 	}
-	if (randomNumber == 2) {
+	if (randomNumber == 3) {
 		r.style.setProperty('--main-bg-color', '#D4FFDF');
 		r.style.setProperty('--main-text-color', '#333');
 		r.style.setProperty('--style-text-color', '#990099');
 	}
-}
+};
