7384f2b13b0baf419691cc749a4e6734ae04e7a5
Justin <justin@Alma.attlocal.net>
2021-02-16 21:33:09 -0800

    Two snack colors.

--- js/randColor.js
+++ js/randColor.js
@@ -1,22 +1,35 @@
 // Get the root element
 var r = document.querySelector(':root');
 
-randomNumber = Math.round(Math.random()*3);
+randomNumber = Math.round(Math.random()*1);
 
 window.onload = function() {
-	if (randomNumber == 0 || randomNumber == 1) {
+	if (randomNumber == 0) {
 		r.style.setProperty('--main-bg-color', '#FFFFCC');
 		r.style.setProperty('--main-text-color', '#333');
 		r.style.setProperty('--style-text-color', '#FF3300');
 	}
-	if (randomNumber == 2) {
-		r.style.setProperty('--main-bg-color', '#EAFFFD');
+	if (randomNumber == 1) {
+		r.style.setProperty('--main-bg-color', '#DDF6DD');
 		r.style.setProperty('--main-text-color', '#333');
 		r.style.setProperty('--style-text-color', '#EE0066');
 	}
-	if (randomNumber == 3) {
-		r.style.setProperty('--main-bg-color', '#DDF6DD');
-		r.style.setProperty('--main-text-color', '#333');
-		r.style.setProperty('--style-text-color', '#990099');
-	}
 };
+
+// window.onload = function() {
+// 	if (randomNumber == 0) {
+// 		r.style.setProperty('--main-bg-color', '#FFFFCC');
+// 		r.style.setProperty('--main-text-color', '#333');
+// 		r.style.setProperty('--style-text-color', '#FF3300');
+// 	}
+// 	if (randomNumber == 2) {
+// 		r.style.setProperty('--main-bg-color', '#EAFFFD');
+// 		r.style.setProperty('--main-text-color', '#333');
+// 		r.style.setProperty('--style-text-color', '#EE0066');
+// 	}
+// 	if (randomNumber == 3) {
+// 		r.style.setProperty('--main-bg-color', '#DDF6DD');
+// 		r.style.setProperty('--main-text-color', '#333');
+// 		r.style.setProperty('--style-text-color', '#990099');
+// 	}
+// };
